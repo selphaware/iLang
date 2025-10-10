a3f417d60b6bce834275bd7df93afea3c1ac1744
selphaware <uahmad3013@outlook.com>
2025-10-10 21:32:59 +0100

    test disassemble executable file

--- tests/compile_asm.sh
+++ tests/compile_asm.sh
@@ -3,3 +3,6 @@ gcc -S -O2 -masm=intel -fverbose-asm dataframe.c -o dataframe.s
 
 rem compile from ASM to EXE
 gcc dataframe.s -o dataframe.exe
+
+rem disassemble EXE to ASM
+rem objdump -D -M intel myprog.exe > myprog.asm
